ed6383cace2bd9445f3f6811f4a5cd0cecf4121a
asanametove <acanaka@gmail.com>
2017-09-13 00:20:18 +0300

    calculator done

--- src/index.js
+++ src/index.js
@@ -1,58 +1,107 @@
-class Calculator {
-
-    sum(...args) {
-        return args.reduce((acc, curr) => {
-            return acc += curr;
-        })
+export class Graph {
+    static buildGraph(string) {
+        let res = new Node(null);
+        return this.split(string, res);
     }
 
-    substract( ...args) {
-        return args.slice().sort((a, b) => {
-            return a === b ? 0 : b - a;
-        }).reduce( (acc, curr) => {
-            return acc -= curr;
+    static split(string, head) {
+        this.operators = [
+            new Operator('+', 1),
+            new Operator('-', 1),
+            new Operator('*', 2),
+            new Operator('/', 2),
+        ];
+
+        let splitIndex = Number.MIN_VALUE,
+            priority = Number.MAX_VALUE,
+            left = null,
+            right = null;
+
+        this.operators.map((operator) => {
+            let tempIndex = string.lastIndexOf(operator.sign);
+
+            if (tempIndex === -1)
+                return;
+
+            if (tempIndex > splitIndex && operator.priority <= priority) {
+                splitIndex = tempIndex;
+                priority = operator.priority;
+                head.value = operator.sign;
+            }
         });
 
 
-        //return a > b ? a - b : b - a;
-    }
-
-    divide(a,b) {
-        if( b!== 0 ){
-            return a/b
+        if (splitIndex !== Number.MIN_VALUE) {
+            left = string.slice(0, splitIndex);
+            right = string.slice(splitIndex + 1, string.length);
+            head.left = this.split(left, new Node(null));
+            head.right = this.split(right, new Node(null));
+            return head;
         } else {
-            throw new TypeError('Second number shouldnt be equal 0')
+            head.value = string;
+            return head;
         }
     }
 
+    static calcChilds(node) {
+        let left = +node.left.value;
+        let right = +node.right.value;
+        let res;
 
+        switch (node.value) {
+            case '+':
+                res = left + right;
+                break;
+            case '-':
+                res = left - right;
+                break;
+            case '*':
+                res = left * right;
+                break;
+            case '/':
+                res = left / right;
+                break;
+            default:
+                throw new Error('Unknown operation');
+        }
 
-    parse(str) {
-        return str
-            .split('+')
-            .map(el => el.split('-')
-                .reduce((acc, curr) =>curr > acc ? curr - acc: acc -curr))
-            .map(el => parseInt(el))
-        //.reduce((acc , curr) => acc += curr)
-
+        return res;
     }
-    calc(str) {
 
-        return this.sum(...this.parse(str));
+    static resolveGraph(head) {
+        if (head) {
+            this.resolveGraph(head.left);
+            this.resolveGraph(head.right);
 
+            if (!Number.isFinite(+head.value)) {
+                head.value = this.calcChilds(head);
+            }
+            return head.value;
+        }
     }
 }
 
-
-export const parse = (str) => {
-
-    return str
-        .split('+')
-        .map(el => el.split('-')
-            .reduce((acc, curr) =>curr > acc ? curr - acc: acc -curr))
-        .map(el => parseInt(el))
-        //.reduce((acc , curr) => acc += curr)
-
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.right = null;
+        this.left = null;
+    }
 }
 
+class Operator {
+    constructor(sign, priority) {
+        this.sign = sign;
+        this.priority = priority;
+    }
+}
+
+export class Calculator {
+    static calculate(expression) {
+        let graph = Graph.buildGraph(expression);
+        return Graph.resolveGraph(graph);
+    }
+}
+
+// export default Graph;
 export default Calculator;

47ced6197755e06465cd591daafe85807968b3ec
asanametove <acanaka@gmail.com>
2017-09-12 22:32:09 +0300

    my own

--- src/index.js
+++ src/index.js
@@ -1,3 +1,66 @@
-class Calculator {}
+class Calculator {
+    // sum()
+
+    substract(...args) {
+        // return a - b;
+        // a > b ? a - b : b - a;
+        return args
+            .slice()
+            .sort((a,b) =>  {
+            return a === b ? 0: b - a
+            })
+            .reduce((acc, cur) => {
+
+                return acc -= cur;
+            })
+
+
+    }
+
+    sum(...args) {
+        // let res = 0;
+        // for(let i = 0; i < arguments.length; i++) {
+        //     res += arguments[i];
+        // }
+        // return res;
+
+        return args.reduce((acc, cur) => {
+            return acc += cur;
+        })
+    }
+
+    divide(a, b) {
+        if(!b) {
+            throw new Error('Dividing by zero');
+        } else {
+            return a / b;
+        }
+    }
+
+    calculate(string) {
+        // return parse(string)
+        //     .map((str) => parseInt(str))
+        //     .reduce( (acc, cur) => {
+        //     return acc += cur;
+        // });
+        return parse(string);
+    }
+}
+
+export const parse = (string) => {
+    let res = 0;
+    string
+        .split('+')
+        .map( sum => {
+            sum.split('-')
+                .map( div => {
+                    if(isFinite(div))
+                        res -= +div;
+                });
+            if(isFinite(sum))
+                res += +sum;
+        });
+    return res;
+};
 
 export default Calculator;
--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,7 @@ import {spy, stub} from 'sinon';
 import sinonChai from 'sinon-chai';
 
 import Calculator from '../src/';
+import {parse} from '../src/'
 
 chai.use(sinonChai);
 
@@ -13,7 +14,66 @@ describe('Calculator', function () {
         sut = new Calculator();
     });
 
-    it('should ', () => {
+    // it('should sum two numbers passed ', () => {
+    //     const expected = 5;
+    //     const actual = sut.sum(2, 3);
+    //     expect(expected).to.equal(actual);
+    // });
 
+    it('should sum all numbers passed ', () => {
+        const expected = 10;
+        const actual = sut.sum(1, 2, 3, 4);
+        expect(expected).to.equal(actual);
+    });
+
+
+    // it('should substract numbers', () => {
+    //     const expected = 1;
+    //     const actual = sut.substract(2, 1);
+    //     expect(expected).to.equal(actual)
+    // })
+
+    it('should substract numbers', () => {
+        const expected = 0;
+        const actual = sut.substract(1,2,3);
+        expect(expected).to.equal(actual)
+    });
+
+    it('should divide numbers', () => {
+        const expected = 10;
+        const actual = sut.divide(100, 10);
+        expect(expected).to.equal(actual);
+    });
+
+    it('should throw error when divide numbers', () => {
+        expect(sut.divide.bind(null, 1, 0)).to.throw();
+    });
+
+    // it('should calculate passed string', () => {
+    //     const expected = 10;
+    //     const actual = sut.calculate('1+2+3+4');
+    //     expect(expected).to.equal(actual);
+    // })
+    //
+    // it('should calculate passed string', () => {
+    //     const expected = 5;
+    //     const actual = sut.calculate('1+2+3+4-5');
+    //     expect(expected).to.equal(actual);
+    // })
+
+
+});
+
+describe('Parse', () => {
+    let sut;
+
+    beforeEach( () => {
+        sut = parse;
+    });
+
+    it('should parse str and array', () => {
+        const actual = sut('10+2');
+        const expected = [10, 2];
+        expect(expected).to.have.all.members(actual);
     });
 });
